886b2b601f192bb4c6fed31240ce72ddeb94a26d
banyedeme <banyedeme0@gmail.com>
2023-12-22 16:22:29 +0000

    Puppet configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# This Puppet manifest creates a file in /tmp/school with specific permissions, owner, group, and content.
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
