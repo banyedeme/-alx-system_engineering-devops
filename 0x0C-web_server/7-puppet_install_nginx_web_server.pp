eef5124a387fa2e51b1ebb28d10b58e240b8be0d
banyedeme <banyedeme0@gmail.com>
2024-01-04 15:37:41 +0000

    installing and configuring web server using nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Installs and Configures Nginx server with Puppet Manifest
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://floormatsavvy.com/contact/ permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
